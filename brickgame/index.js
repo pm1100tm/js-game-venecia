75c819d2f76bc73970a8d15903f6d9da3fab287a
dev-swd <tladnjsen@gmail.com>
2020-09-18 01:28:24 +0900

    setting canvas with css

---
+++ brickgame/index.js
@@ -0,0 +1,7 @@
+/** @format */
+
+// 캔버스 엘리먼트 참조 변수
+const canvas = document.getElementById('canvas');
+
+// 실질적으로 캔버스를 그리기 위한 2d rendering context 를 ctx 변수에 저장
+const ctx = canvas.getContext('2d');
