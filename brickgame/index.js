0d8ae66963a289647750ec0aeecdbdb5b4cda748
dev-swd <tladnjsen@gmail.com>
2020-09-18 02:16:20 +0900

    drawBall, draw() 메서드 추가

--- brickgame/index.js
+++ brickgame/index.js
@@ -6,26 +6,34 @@ const canvas = document.getElementById('canvas');
 // 실질적으로 캔버스를 그리기 위한 2d rendering context 를 ctx 변수에 저장
 const ctx = canvas.getContext('2d');
 
-// 캔버스에 빨간색 사각형 그리기
-// beginPath() 와 closePath() 사이에 모든 명령어가 들어간다.
-// rect : 인자 left, top, width, height
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = '#FF0000';
-ctx.fill();
-ctx.closePath();
+// 원의 초기 위치 x, y 값
+let x = canvas.clientWidth / 2;
+let y = canvas.clientHeight - 30;
 
-// 캔버스에 사각형 그리기
-ctx.beginPath();
-// arc - 인자 : 원의 중심 x, y, 원읜 반지름, 시작 각도와 끝, 그리는 방향(true: 반시계)
-ctx.arc(240, 160, 20, Math.PI * 2, false);
-ctx.fillStyle = 'green';
-ctx.fill();
-ctx.closePath();
+// 원의 이동할 값 dx, dy
+let dx = 2;
+let dy = -2;
 
-// 캔버스에 테두리만 있는 사각형 그리기
-ctx.beginPath();
-ctx.rect(160, 10, 100, 40);
-ctx.strokeStyle = 'rgba(0, 0, 255, 0.5)';
-ctx.stroke();
-ctx.closePath();
+// 공 그리기 메서드
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(x, y, 10, 0, Math.PI * 2);
+  ctx.fillStyle = '#0095DD';
+  ctx.fill();
+  ctx.closePath();
+}
+
+function draw() {
+  // 캔버스의 내용을 지워는 메서드 clearPath() 4개의 파라미터.
+  // 직사각형 캔버스의 좌상단 모서리 (x, y), 우하단 모서리 (canvas width, height)
+  // 지정한 영역 안에 있는 것들은 전부 지워지게 됨.
+  ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+  drawBall();
+  x += dx;
+  y += dy;
+}
+
+// JavaScript 타이밍 함수인
+// setInterval()나 requestAnimationFrame()를 이용하면
+// 함수를 몇번이고 계속 반복해서 실행할 수 있습니다..
+setInterval(draw, 10);
